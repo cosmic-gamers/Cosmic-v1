4efbd42941543a286c88be3701fdfa661e45ec30
cosmic-gamers <139851008+cosmic-gamers@users.noreply.github.com>
2023-08-29 09:50:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -123,3 +123,27 @@ server.on("listening", () => {
 server.listen({
   port: process.env.PORT,
 });
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD62xmVkkhZD0aWtv8fwezw5wAHoOAuINI",
+    authDomain: "cosmic-55513.firebaseapp.com",
+    projectId: "cosmic-55513",
+    storageBucket: "cosmic-55513.appspot.com",
+    messagingSenderId: "876836656622",
+    appId: "1:876836656622:web:c32cf6d21cfba270fc6aff",
+    measurementId: "G-WR09YS5YHN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
